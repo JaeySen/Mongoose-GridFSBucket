5607fb6a1b67c6db9dea323021fa03f5a4458dfc
JaeySen <longhientranjjj@gmail.com>
2023-10-10 13:55:31 +0700

    Upgrade to GridFSBucket, fix Uploader

--- models/files.model.js
+++ models/files.model.js
@@ -15,4 +15,4 @@ const fileSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('File', fileSchema);
+module.exports = mongoose.model('File', fileSchema, "files");
--- routings/v1/upload/documents-uploader.js
+++ routings/v1/upload/documents-uploader.js
@@ -3,34 +3,36 @@ const logger = require('../../../config/logger.config');
 
 // Logic
 const mongoose = require("mongoose");
-const Grid = require('gridfs-stream');
 
 //models
 const Files = require('../../../models/files.model');
 
 module.exports = router => {
     const conn = mongoose.connection;
-    Grid.mongo = mongoose.mongo;
+    // Grid.mongo = mongoose.mongo;
+    // const gridFSBucket = new mongoose.mongo.GridFSBucket(conn.db);
     let gfs;
 
     conn.once("open", () => {
-        gfs = Grid(conn.db);
+        // gfs = Grid(conn.db);
+        gfs = new mongoose.mongo.GridFSBucket(conn.db);
 
         router.post('/bucket/upload', (req, res) => {
             let {
                 file
             } = req.files;
-            let writeStream = gfs.createWriteStream({
+            let writeStream = gfs.openUploadStream({
                 filename: `${file.name}`,
                 mode: 'w',
                 content_type: file.mimetype
             });
-            writeStream.on('close', function (uploadedFile) {
+            writeStream.on('finish', (uploadedFile) => {
+                console.log(uploadedFile)
                 Files.create({
                         doc_id: uploadedFile._id,
                         length: uploadedFile.length,
-                        name: uploadedFile.filename,
-                        type: uploadedFile.contentType
+                        name: uploadedFile.filename.filename,
+                        type: uploadedFile.filename.contentType
                     })
                     .then(_ => res.json({
                         success: true,
